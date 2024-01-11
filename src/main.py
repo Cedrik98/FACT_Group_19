2c29289430e6856cd0380189fa4f236add67362b
Jakob Kaiser <jakob.j.jeep@gmail.com>
2024-01-11 20:01:07 +0100

    --amend

--- src/main.py
+++ src/main.py
@@ -1,25 +1,26 @@
 import argparse
 
-import torch
-from textattack.model.wrappers import HuggingFaceDataset, HuggingFaceModelWrapper
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
-
-
-def load_dataset(dataset_name, _):
-    return "a", "b", "c"
+# import torch
+# from textattack.model.wrappers import HuggingFaceDataset, HuggingFaceModelWrapper
+# from transformers import AutoModelForSequenceClassification, AutoTokenizer
+#
+#
+# def load_dataset(dataset_name, _):
+#     return "a", "b", "c"
 
 
 def run(args):
-    _, dataset_test, categories = load_dataset(args.dataset, args.seed_dataset)
-    dataset = HuggingFaceDataset(dataset_test)
-
-    model = AutoModelForSequenceClassification.from_pretrained(args.model)
-    tokenizer = AutoTokenizer.from_pretrained(args.model)
-
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-    model.to(device)
-
-    model_wrapper = HuggingFaceModelWrapper(model, tokenizer)
+    pass
+    # _, dataset_test, categories = load_dataset(args.dataset, args.seed_dataset)
+    # dataset = HuggingFaceDataset(dataset_test)
+    #
+    # model = AutoModelForSequenceClassification.from_pretrained(args.model)
+    # tokenizer = AutoTokenizer.from_pretrained(args.model)
+    #
+    # device = "cuda" if torch.cuda.is_available() else "cpu"
+    # model.to(device)
+    #
+    # model_wrapper = HuggingFaceModelWrapper(model, tokenizer)
 
 
 if __name__ == "__main__":
