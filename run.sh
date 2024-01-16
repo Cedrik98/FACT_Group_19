c44f63c9acda98975644f9e9fce4236aba6a9413
Jakob Kaiser <jakob.j.jeep@gmail.com>
2024-01-17 00:19:19 +0100

    added help to run script

--- run.sh
+++ run.sh
@@ -3,12 +3,22 @@
 RED='\033[0;31m'
 NC='\033[0m'
 
-if [[ $1 == !(train|experiment|evaluate) ]]; then
-    echo -e "${RED}[!]${NC} Not a valid argument ${1}\n"
+function usage() {
     echo -e "Usage:
+    help       \t\t\t- print this help message
     train      \t\t\t- train language models
     experiment \t\t\t- run various experiments
     evaluate   \t\t\t- run evaluation on experiment results"
+}
+
+if [[ $1 == ?(help|--help|-h) ]]; then
+    usage
+    exit 0
+fi
+
+if [[ $1 == !(train|experiment|evaluate) ]]; then
+    echo -e "${RED}[!]${NC} Not a valid argument ${1}\n"
+    usage
     exit 1
 fi
 
